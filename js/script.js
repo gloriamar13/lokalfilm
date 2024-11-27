d5a0eef71ecf4c5387eb39fda954c111f195a897
DwiArfian12 <dwiarfiann@gmail.com>
2024-11-27 23:33:40 +0700

    feat: update navigation and button actions for improved user experience

--- js/script.js
+++ js/script.js
@@ -23,21 +23,4 @@ document.addEventListener("DOMContentLoaded", function () {
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return regex.test(email);
     }
-
-    // Event handler untuk navigasi scroll halus
-    const navLinks = document.querySelectorAll("nav ul li a");
-    navLinks.forEach(link => {
-        link.addEventListener("click", function (event) {
-            event.preventDefault();
-            const targetId = this.getAttribute("href").substring(1);
-            const targetElement = document.getElementById(targetId);
-
-            if (targetElement) {
-                window.scrollTo({
-                    top: targetElement.offsetTop - 100, // Sesuaikan offset jika ada header tetap
-                    behavior: "smooth"
-                });
-            }
-        });
-    });
 });
